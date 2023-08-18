39fff19b033e290b92a106085236f03e18611939
home <gkadudgnl@gmail.com>
2023-08-19 08:59:22 +0900

    Springboot Filter 추가, login thymeleaf(타임리프)

--- src/main/java/com/fastcampus/ch2/LoginController.java
+++ src/main/java/com/fastcampus/ch2/LoginController.java
@@ -1,12 +1,12 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.net.URLEncoder;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/login")
@@ -22,7 +22,7 @@ public class LoginController {
     //    @RequestMapping(value="/login/login", method= {RequestMethod.GET,RequestMethod.POST})
     //    @RequestMapping(value="/login/login", method= RequestMethod.POST) // 아래의 라인과 동일
     @PostMapping("/login")
-    public String login(String id, String pwd, Model model) throws Exception{
+    public String login(HttpServletRequest req, String id, String pwd, RedirectAttributes model) throws Exception{
         // 1. id, pwd를 확인
         if(loginCheck(id,pwd)) {
             // 2. 일치하면, userInfo.html
@@ -31,8 +31,12 @@ public class LoginController {
             return "userInfo" ;  // userInfo.html
         } else {
             //    일치하지 않으면, login.html로 이동
-            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
-            return "redirect:/login/login?msg="+msg;  // GET - URL재작성
+            String msg = "id 또는 pwd가 일치하지 않습니다.";
+            model.addAttribute("msg", msg);
+            model.addFlashAttribute("msg", "일회용 메시지");
+            req.setAttribute("msg", "request에 저장된 msg");
+//            return "redirect:/login/login?msg="+msg;  // GET - URL재작성
+            return "redirect:/login/login";
         }
     }
 
